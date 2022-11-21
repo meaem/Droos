d1955316fd49b24647fe52f6abeff84a07098499
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-11-21 15:31:44 +0200

    initial version

---
+++ app/src/main/java/com/aabdelaal/droos/ui/firebaseLogin/FirebaseLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.aabdelaal.droos.ui.firebaseLogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aabdelaal.droos.R
+
+class FirebaseLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_login)
+    }
+}
